c91fbc0e3583452f736ab4107dd4e8940ed54f0d
Alexander Diewald <Diewi@diewald-net.com>
2017-08-07 14:55:09 +0200

    Add variant detection & releasetools from sony-common.
    Change-Id: I70b4b0da47218d6a0c56d8ea182e6d7dece84261
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- msm8974_omni.mk
+++ msm8974_omni.mk
@@ -24,5 +24,18 @@ USE_CAMERA_STUB := true
 # msm8974 needs a custom liblights implementation
 TARGET_PROVIDES_LIBLIGHTS := true
 
+# Variant linking script
+PRODUCT_COPY_FILES += \
+    device/sony/msm8974-common/releasetools/updater.sh:utilities/updater.sh
+
+# SELinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Exclude these from build.prop, they are set by libinit
+PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
+    ro.product.model \
+    ro.product.device
+
 # inherit from msm8974.mk
 include device/sony/msm8974-common/msm8974.mk
