175fa96bacdce25fec6f0272479cbe89e8afd1e1
Alexander Diewald <Diewi@diewald-net.com>
2017-11-18 13:26:00 +0100

    sony-8974: GPS: Import the oppo8974 GPS HAL.
    * The GPS HAL for sony-msm8974 has been been using the blobs from
      stock. Use a source-based HAL instead.
    * Taken from oppo/msm8974-common.
    
    Change-Id: I552ffc2d3dcdeb6c50b6069cd95dea37e74e6563
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>
    Signed-off-by: nailyk-fr <jenkins@nailyk.fr>
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,3 +1,20 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2015 The OmniRom Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -9,3 +26,5 @@ $(gps_conf_dir)/$(gps_debug_conf):
 		ln -sf $(gps_conf) $(TARGET_OUT_ETC)/$(gps_debug_conf)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(gps_conf_dir)/$(gps_debug_conf)
+
+include $(call first-makefiles-under,$(call my-dir))
