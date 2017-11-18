fb224c4000b4d1f32bc88f7a4741fa8a576bb6b7
Alexander Diewald <Diewi@diewald-net.com>
2017-11-18 12:15:55 +0100

    Revert "sony: msm8974: Use audio_effects from common."
    This reverts commit f8667a34b531c777a61f86928d48dd0b621ea4c0.
    
    Change-Id: Ib34b4bcc0ab910bfcef19ec1569424ab1992f5b5

--- msm8974.mk
+++ msm8974.mk
@@ -12,6 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Audio configuration for msm8974 sony boards. Must be defined before including
+# common, since PRODUCT_COPY_FILES entries cannot be overridden and the common
+# version would be used otherwise.
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+
 COMMON_PATH := device/sony/msm8974-common
 
 # Include msm8974-common system properties
