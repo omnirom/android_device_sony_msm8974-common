5c0075542bc486bc2efbf942fd50e9b0e14d2acb
nailyk-fr <nailyk_git@nailyk.fr>
2017-11-29 21:00:53 +0100

    [testing] GPS updates
    Change-Id: Iddf7b54087b754492d62f9eef5227bdde89bac28
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -14,11 +14,11 @@ LOCAL_CFLAGS += -DPDK_FEATURE_SET
 endif
 
 LOCAL_SHARED_LIBRARIES := \
+    liblog \
     libutils \
     libcutils \
     libgps.utils \
-    libdl \
-    liblog
+    libdl
 
 LOCAL_SRC_FILES += \
     LocApiBase.cpp \
