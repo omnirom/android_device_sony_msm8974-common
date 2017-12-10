1768f42ce8d6676b0ddbf24fba9229556fa66ff6
nailyk-fr <nailyk_git@nailyk.fr>
2017-12-10 17:33:58 +0100

    msm8974-common: Build: Use 8994 gps
    Change-Id: Ib14ae1bb04fe2ed893d34da5cd8250adccfb7d55
    Signed-off-by: nailyk-fr <nailyk_git@nailyk.fr>

--- hidl.mk
+++ hidl.mk
@@ -28,7 +28,9 @@ PRODUCT_PACKAGES += \
  	
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+	android.hardware.gnss@1.0-impl \
+	android.hardware.gnss@1.0 \
+	android.hardware.gnss-V1.0-java
  	
 # Keymaster
 PRODUCT_PACKAGES += \
