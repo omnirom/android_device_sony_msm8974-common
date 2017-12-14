e93c0d230dc561845d71dabd2a97444788530ce4
mcgi5sr2 <mcgi5sr2@gmail.com>
2017-12-14 16:11:38 +0000

    Add Gatekeeper to hidl and treble
    Change-Id: Ieaca8eccafcc6a276772568dd2cc115fea021ab4

--- hidl.mk
+++ hidl.mk
@@ -26,6 +26,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.broadcastradio@1.0-impl
  	
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
