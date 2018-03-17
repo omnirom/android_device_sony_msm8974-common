7d6da8ee4ae84c0a22d52d735ba2e3109e1dcd64
Mickybart <mickybart@pygoscelis.org>
2018-03-17 14:02:28 +0100

    sony-8974: Disable Metadata storage in buffers.
    Legacy sony-msm8974 shall not store metadata in buffers since wrong
    code paths would be entered in stagefright that break video processing.
    
    Context:
    Storing the metadata of captured frames in the buffer breaks recording
    for legacy devices, since "wrong" code paths are followed in this case.
    This behaviour is hardcoded by a default parameter in the CameraSource
    and CameraSourceTimelapse constructors.
    
    This change allows to falsify TARGET_STORES_METADATA_IN_BUFFERS such
    that the metadata storage can be controlled in from device makefiles.
    
    Change-Id: I7d676b20e5ab2474ae966be47112daa06b7d20fd
    Signed-off-by: Alexander Diewald <Diewi@diewald-net.com>

--- msm8974.mk
+++ msm8974.mk
@@ -85,11 +85,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Media
-PRODUCT_PACKAGES += \
-    qcmediaplayer
+#PRODUCT_PACKAGES += \
+#    qcmediaplayer
 
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
+#PRODUCT_BOOT_JARS += \
+#    qcmediaplayer
 
 # Permissions
 PRODUCT_COPY_FILES += \
