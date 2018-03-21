a3ffa020f1e7cf5e811c8eb16af115c50d7b2e72
Adrian DC <radian.dc@gmail.com>
2018-03-22 00:32:41 +0100

    camera: libstagefright: Disable camcorder metadata
     * Refer to the following commit:
    
        Author: Adrian DC <radian.dc@gmail.com>
        Date:   Tue, 20 Mar 2018 23:48:08 +0100
        libstagefright: Support disabling metadata with a property
        Change-Id: Ia5f3bd30cd24bd2d10c9c145c91d4b616f4edb44
    
    Change-Id: Idfda05fa984181c931ef324788985e2aa2217ec2

--- systemprop.mk
+++ systemprop.mk
@@ -63,7 +63,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qc.sdk.gestures.camera=false \
     camera.disable_zsl_mode=1 \
     persist.camera.HAL3.enabled=0 \
-    camera2.portability.force_api=1
+    camera2.portability.force_api=1 \
+    debug.camcorder.disablemeta=true
 
 # Misc
 PRODUCT_PROPERTY_OVERRIDES += \
